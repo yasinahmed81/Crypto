ba63b594c29f95b76eac6512bcc49ec9d45bb2d2
Yasin <yasinahmed81@gmail.com>
2021-02-06 13:13:54 +0000

    Project cleanup

--- Crypto/CurrencyViewController.swift
+++ Crypto/CurrencyViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CurrencyViewController.swift
 //  Crypto
 //
 //  Created by Yasin Ahmed on 06/02/2021.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class CurrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
