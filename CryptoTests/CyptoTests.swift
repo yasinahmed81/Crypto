52b781663b93455c34d54b17b33d7f7b2028126a
Yasin <yasinahmed81@gmail.com>
2025-05-18 15:13:02 +0100

    Adds CryptoTests

---
+++ CryptoTests/CyptoTests.swift
@@ -0,0 +1,13 @@
+//
+//  CyptoTests.swift
+//  Crypto
+//
+//  Created by Yasin Ahmed on 18/05/2025.
+//
+
+@testable import Crypto
+import XCTest
+
+final class CryptoTests: XCTestCase {
+    func testExample() {}
+}
